e188eb60c1e5e8597d48c445164c106c1391888d
JaGenn <dzhan.dzhan.2000@mail.ru>
2025-03-06 22:57:44 +0300

    Added ExceptionHandler

---
+++ src/main/java/org/example/Utils/ExceptionHandler.java
@@ -0,0 +1,47 @@
+package org.example.Utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.dto.ErrorResponseDto;
+import org.example.exception.DataBaseOperationErrorException;
+import org.example.exception.NotFoundException;
+
+import java.io.IOException;
+
+import static jakarta.servlet.http.HttpServletResponse.*;
+import static jakarta.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+
+@WebFilter("/*")
+public class ExceptionHandler extends HttpFilter {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
+        try {
+            super.doFilter(req, res, chain);
+        }
+        catch (DataBaseOperationErrorException e) {
+            writeErrorResponse(res, SC_INTERNAL_SERVER_ERROR, e);
+        }
+        catch (NotFoundException e) {
+            writeErrorResponse(res, SC_NOT_FOUND, e);
+        }
+    }
+
+    private void writeErrorResponse(HttpServletResponse response, int errorCode, RuntimeException e) throws IOException {
+        response.setStatus(errorCode);
+
+        objectMapper.writeValue(response.getWriter(), new ErrorResponseDto(
+                errorCode,
+                e.getMessage()
+        ));
+    }
+}
